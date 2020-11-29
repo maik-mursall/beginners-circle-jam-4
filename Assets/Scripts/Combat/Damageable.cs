92b3ef7cdea6b9aa8f365e0c56a70cef2770a724
Maik Mursall <maikmursall@yahoo.de>
2020-11-29 02:12:46 +0100

    Added Damageable

---
+++ Assets/Scripts/Combat/Damageable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+namespace Combat
+{
+    public class Damageable : MonoBehaviour
+    {
+        public float Health { get; private set; } = 50f;
+        [SerializeField] private float maxHealth = 50f;
+
+        public event EventHandler Died;
+        public event EventHandler Damaged;
+        public event EventHandler Healed;
+
+        private void OnEnable()
+        {
+            Health = maxHealth;
+        }
+
+        public void Heal(float amount)
+        {
+            Health = Mathf.Clamp(Health + amount, 0f, maxHealth);
+            OnHeal(EventArgs.Empty);
+        }
+
+        public void TakeDamage(float damage)
+        {
+            Health -= damage;
+            if (Health <= 0f)
+            {
+                OnDeath(EventArgs.Empty);
+            }
+            else
+            {
+                OnDamage(EventArgs.Empty);
+            }
+        }
+
+        private void OnDamage(EventArgs e)
+        {
+            Damaged?.Invoke(this, e);
+        }
+
+        private void OnDeath(EventArgs e)
+        {
+            Died?.Invoke(this, e);
+        }
+
+        private void OnHeal(EventArgs e)
+        {
+            Healed?.Invoke(this, e);
+        }
+    }
+}
