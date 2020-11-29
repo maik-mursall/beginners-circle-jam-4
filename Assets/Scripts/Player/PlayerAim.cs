1ebfabf5b4759d266d0863022ea0306151395a6d
Maik Mursall <maikmursall@yahoo.de>
2020-11-29 01:56:32 +0100

    Added Basic Movement and Aim

---
+++ Assets/Scripts/Player/PlayerAim.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerAim : MonoBehaviour
+    {
+        private Camera _mainCamera;
+
+        private void Start()
+        {
+            _mainCamera = Camera.main;
+        
+        
+        }
+
+        void Update()
+        {
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+        
+            Plane plane = new Plane(Vector3.up, Vector3.up * transform.position.y);
+            if (plane.Raycast(ray, out float distance))
+            {
+                Vector3 target = ray.GetPoint(distance);
+                Vector3 direction = target - transform.position;
+                float rotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Euler(0, rotation, 0);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        private CharacterController _characterController;
+
+        private Vector2 _playerInput = Vector2.zero;
+
+        [SerializeField] private float moveSpeed = 10f;
+        private void Start()
+        {
+            _characterController = GetComponent<CharacterController>();
+        }
+
+        private void Update()
+        {
+            _playerInput.x = Input.GetAxisRaw("Horizontal");
+            _playerInput.y = Input.GetAxisRaw("Vertical");
+        }
+
+        private void FixedUpdate()
+        {
+            var normalizedPlayerInput = _playerInput.normalized;
+            _characterController.Move(new Vector3(normalizedPlayerInput.x, 0f, normalizedPlayerInput.y) * (moveSpeed * Time.fixedDeltaTime));
+        }
+    }
+}
