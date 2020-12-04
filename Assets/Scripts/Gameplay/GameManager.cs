c0573c3f66f3b6a3fba15d8d6672f42be03cb765
Maik Mursall <maikmursall@yahoo.de>
2020-12-05 00:10:26 +0100

    Simple Hypemeter

---
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Gameplay
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance;
+
+        private void Awake()
+        {
+            if (Instance) DestroyImmediate(gameObject);
+
+            Instance = this;
+        }
+
+        public void GameOver()
+        {
+            Debug.Log("Game over qq");
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/HypeMeter/HypeMeterUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gameplay.HypeMeter
+{
+    public class HypeMeterUI : MonoBehaviour
+    {
+        private HypeMeter _hypeMeter;
+        [SerializeField] private Slider slider;
+
+        void Start()
+        {
+            _hypeMeter = HypeMeter.Instance;
+            slider.value = _hypeMeter.HypePercent;
+        }
+
+        void Update()
+        {
+            slider.value = _hypeMeter.HypePercent;
+        }
+    }
+}
