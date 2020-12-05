587d43430c1fe29ac665ba441ee3b51b3f680fbf
Maik Mursall <maikmursall@yahoo.de>
2020-12-05 19:12:07 +0100

    Good enough Health Bar looking at camera for now

---
+++ Assets/Scripts/Utils/LookAtCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public class LookAtCamera : MonoBehaviour
+    {
+        private Transform _cameraTransform;
+
+        private void Start()
+        {
+            _cameraTransform = Camera.main.transform;
+        }
+
+        private void LateUpdate()
+        {
+            transform.rotation = _cameraTransform.rotation * Quaternion.Euler(0f, 180f, 0f);
+        }
+    }
+}
