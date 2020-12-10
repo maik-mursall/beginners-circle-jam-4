9684feb4a70d6a6ecf8725c986e6f3ce492f3c9b
Maik Mursall <maikmursall@yahoo.de>
2020-12-11 00:16:23 +0100

    i want die

--- Assets/Scripts/Enemy/EnemyLaserCombat.cs
+++ Assets/Scripts/Enemy/EnemyLaserCombat.cs
@@ -1,22 +1,26 @@
 ﻿using System.Collections;
+using DG.Tweening;
 using Gameplay.HypeMeter;
 using UnityEngine;
 
 namespace Enemy
 {
-    public class EnemyMeleeCombat : MonoBehaviour
+    public class EnemyLaserCombat : MonoBehaviour
     {
-        [SerializeField] private Animator animator;
-        [SerializeField] private SphereCollider attackCollider;
+        [SerializeField] private LineRenderer lineRenderer;
         
         [SerializeField] private float damage = 20f;
         [SerializeField] private float attackRange = 20f;
-        [SerializeField] private float attackSpeed = 20f;
         [SerializeField] private float attackMinCooldown = 5f;
         [SerializeField] private float attackMaxCooldown = 10f;
         [SerializeField] private float attackDuration = 5f;
         [SerializeField] private float minGoBackRange = 5f;
         [SerializeField] private float maxGoBackRange = 10f;
+        
+        [SerializeField] private Transform rayTransform;
+        [SerializeField] private float rayRadius;
+        [SerializeField] private LayerMask rayLayerMask;
+        [SerializeField] private Quaternion rayStartRotation;
 
         private EnemyMovement _movement;
 
@@ -29,21 +33,46 @@ namespace Enemy
             _movement.TargetReached += (sender, args) => StartCoroutine(StartCooldownCoroutine());
         }
         
-        private static readonly int IsAttacking = Animator.StringToHash("isAttacking");
-
         private void Update()
         {
             if (_movement.CurrentMoveState != EnemyMovementState.Idling && !(_movement.Target is null) && !_onCooldown && Vector3.Distance(_movement.Target.position, transform.position) <= attackRange)
             {
                 StartAttacking();
             }
+
+            if (lineRenderer.enabled)
+            {
+                ShootLaser();
+            }
+        }
+
+        private void ShootLaser()
+        {
+            lineRenderer.SetPosition(0, rayTransform.position);
+
+            if (Physics.SphereCast(rayTransform.position, rayRadius, rayTransform.forward, out RaycastHit hit,
+                Mathf.Infinity, rayLayerMask))
+            {
+                if (hit.transform.CompareTag("Player"))
+                {
+                    HypeMeter.Instance.AddAbsoluteHype(-damage);
+                    lineRenderer.SetPosition(1, hit.point);
+                }
+            }
+            else
+            {
+                lineRenderer.SetPosition(1, rayTransform.forward * 1000f);
+            }
         }
 
         private void StartAttacking()
         {
-            animator.SetBool(IsAttacking, true);
-            _movement.SetMoveSpeed(attackSpeed);
-            attackCollider.enabled = true;
+            _movement.SetMoveSpeed(0);
+            lineRenderer.enabled = true;
+
+            rayTransform.localRotation = rayStartRotation;
+            rayTransform.DOLocalRotate(Vector3.zero, 4f);
+            
             _onCooldown = true;
 
             StopAllCoroutines();
@@ -52,15 +81,13 @@ namespace Enemy
         
         private void StopAttacking()
         {
-            animator.SetBool(IsAttacking, false);
             _movement.ResetMoveSpeed();
+            lineRenderer.enabled = false;
 
             var randomPosition = Random.insideUnitCircle * Random.Range(minGoBackRange, maxGoBackRange);
             _movement.SetStaticPosition(
                 new Vector3(randomPosition.x, 0f, randomPosition.y)
             );
-            
-            attackCollider.enabled = false;
         }
         
         private IEnumerator StartAttackCoroutine()
